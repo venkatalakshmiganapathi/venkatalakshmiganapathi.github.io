8a63edfe53c1920431ecf523ea475c3aa1a37207
venkatalakshmiganapathi <subadra.rachakonda@gmail.com>
2025-03-01 19:49:52 -0600

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+// This file contains the JavaScript code for the webpage. 
+// You can add functions to add interactivity or manipulate the DOM.
+
+document.addEventListener('DOMContentLoaded', () => {
+    const greeting = document.createElement('h1');
+    greeting.textContent = 'Welcome to My Simple Webpage!';
+    document.body.appendChild(greeting);
+});
